df75c0d63f1ce3d63522b41c088fda3599ee1023
Priyanka <priyanka3007@gmail.com>
2019-01-28 15:23:45 -0800

    First component Layout and font customized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <h1>My burger builder</h1>
+        <h1>My burger builder</h1>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
