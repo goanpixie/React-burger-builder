fd76b203faf48b0bbd4c263a7664c4a98e092cc2
Priyanka <priyanka3007@gmail.com>
2019-01-28 15:33:26 -0800

    setting up BurgerBuilder container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends Component {
   render() {
     return (
       <div>
         <h1>My burger builder</h1>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder></BurgerBuilder>
         </Layout>
       </div>
     );
