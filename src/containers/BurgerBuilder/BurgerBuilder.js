b692abb183f88efa428ba13f7a6f30796cffb4b9
Priyanka <priyanka3007@gmail.com>
2019-01-29 13:11:14 -0800

    Generic method to output dynamic ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,13 +3,21 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-		render () {
-			return (
-				<Aux>
-					<Burger/>
-					<div>Burger Controls</div>
-				</Aux>
-			);
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat:2
+		}
+	}
+	render () {
+		return (
+			<Aux>
+				<Burger ingredients={this.state.ingredients}/>
+				<div>Burger Controls</div>
+			</Aux>
+		);
 	}
 }
 
