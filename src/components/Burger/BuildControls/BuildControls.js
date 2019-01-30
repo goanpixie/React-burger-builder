18399ee4baa9b241a6ba302baadf12019aac9230
Priyanka <priyanka3007@gmail.com>
2019-01-30 12:44:07 -0800

    Fixing bug to accomodate disable order now button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -30,7 +30,8 @@ const buildControls = (props) => (
 			deducted = {() => props.ingredientDeducted(ctrl.type)}
 			disabled = {props.disabled[ctrl.type]}/>
 	))}
-		  <button className={classes.OrderButton}>ORDER NOW</button>
+			<button className={classes.OrderButton}
+				disabled={!props.purchasable}>ORDER NOW</button>
 		</div>
 
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,19 @@ class BurgerBuilder extends Component {
 			cheese: 0,
 			meat:0
 		},
-		totalPrice: 4
+		totalPrice: 4,
+		purchasable:false
+	}
+
+	updatePurchaseState(ingredients) {
+		const sum = Object.keys(ingredients)
+			.map(igKey => {
+				return ingredients[igKey];
+			})
+			.reduce((sum, el) => {
+				return sum + el;
+			}, 0);
+		this.setState({purchasable:sum>0})
 	}
 
 	addIngredientHandler = (type) => {
@@ -30,8 +42,8 @@ class BurgerBuilder extends Component {
 		const priceAddition = INGREDIENT_PRICES[type];
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice + priceAddition;
-		this.setState({totalPrice:newPrice, ingredients:updatedIngredients})
-
+		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+		this.updatePurchaseState(updatedIngredients);
 	}
 
 	removeIngredientHandler = (type) => {
@@ -47,7 +59,8 @@ class BurgerBuilder extends Component {
 		const priceDeduction = INGREDIENT_PRICES[type];
 		const oldPrice = this.state.totalPrice;
 		const newPrice = oldPrice - priceDeduction;
-		this.setState({totalPrice:newPrice, ingredients:updatedIngredients})
+		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+		this.updatePurchaseState(updatedIngredients);
 	}
 	render() {
 		const disabledInfo = {
@@ -63,6 +76,7 @@ class BurgerBuilder extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientDeducted={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
 					price={this.state.totalPrice}/>
 			</Aux>
 		);
