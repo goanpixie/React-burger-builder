cc0240cd16a75934ab7c3974b1b6fdf313de7171
Priyanka <priyanka3007@gmail.com>
2019-02-01 11:19:48 -0800

    Updates

---
+++ precache-manifest.68c753e217964ebc397ca674a7847ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "982a72938a7241a18899",
+    "url": "/React-burger-builder/static/js/runtime~main.982a7293.js"
+  },
+  {
+    "revision": "1003d8369feba7d5dcfb",
+    "url": "/React-burger-builder/static/js/main.1003d836.chunk.js"
+  },
+  {
+    "revision": "5e0515afe0d91bb2da09",
+    "url": "/React-burger-builder/static/js/1.5e0515af.chunk.js"
+  },
+  {
+    "revision": "1003d8369feba7d5dcfb",
+    "url": "/React-burger-builder/static/css/main.0733b392.chunk.css"
+  },
+  {
+    "revision": "f1110fdd94b63724fbbd060739bc1b19",
+    "url": "/React-burger-builder/index.html"
+  }
+];
