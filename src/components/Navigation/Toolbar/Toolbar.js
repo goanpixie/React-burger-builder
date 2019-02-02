0caf40572fae6096ebcd145ac5d11e3c1e790cf8
Priyanka <priyanka3007@gmail.com>
2019-02-01 16:59:17 -0800

    Adding toolbar foldar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const toolbar = (props) => (
+	<header>
+		<div>MENU</div>
+		<div>LOGO</div>
+		<nav>
+			<a>a</a>
+			<a>b</a>
+			<a>c</a>
+			<a>d</a>
+		</nav>
+	</header>
+);
+
+export default toolbar;
