aa5a6226c7c2319f936d023750e9b2d9f51b7bc0
Priyanka <priyanka3007@gmail.com>
2019-02-04 15:28:49 -0800

    Sidedrawer component added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 
 const toolbar = (props) => (
@@ -8,8 +9,7 @@ const toolbar = (props) => (
 		<div>MENU</div>
 		<Logo/>
 		<nav>
-			<a href="#">a</a>
-
+			<NavigationItems/>
 		</nav>
 	</header>
 );
