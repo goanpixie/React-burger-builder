9c16d0ee414646d5354e9e707e40313a97c4a122
Priyanka <priyanka3007@gmail.com>
2019-02-04 15:41:29 -0800

    SideDrawer added to Layout.js

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css'
 
-const sideDrawer = (props) => (
+const sideDrawer = (props) => {
 
 	return (
-		<div>
+		<div className={classes.SideDrawer}>
 			<Logo />
 			<nav>
-				<NavigationItems/>
+				<NavigationItems />
 			</nav>
 		</div>
 	);
-);
+};
 
 export default sideDrawer;
