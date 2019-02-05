8672d718d9577aaf910ddc8316c54815214d850d
Priyanka <priyanka3007@gmail.com>
2019-02-05 11:59:48 -0800

    added backdrop for sideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css'
+import classes from './SideDrawer.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
 
 	return (
-		<div className={classes.SideDrawer}>
-			<Logo height="11%"/>
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		<Aux>
+		<BackDrop show/>
+			<div className={classes.SideDrawer}>
+				<Logo height="11%"/>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
 	);
 };
 
