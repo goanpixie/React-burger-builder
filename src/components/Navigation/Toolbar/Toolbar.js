0fe634a1201464ec0881a2d72c2629003805143d
Priyanka <priyanka3007@gmail.com>
2019-02-04 16:05:14 -0800

    completed sideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,7 @@ const toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
 		<Logo height="80%"/>
-		<nav>
+		<nav className={classes.DesktopOnly}>
 			<NavigationItems/>
 		</nav>
 	</header>
